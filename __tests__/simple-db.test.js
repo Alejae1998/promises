4d4496d1d80aeb1fed6d996f780e5dc6504e3408
Alejae1998 <alejagr1998@gmail.com>
2022-09-19 09:05:40 -0700

    importing to the test

--- __tests__/simple-db.test.js
+++ __tests__/simple-db.test.js
@@ -1,19 +1,16 @@
-const fs = require('fs/promises');
+const fs = require('node:fs/promises');
 const path = require('path');
-
+const SimpleDb = require('../lib/simple-db.js');
+const crypto = require('crypto');
 const { CI, HOME } = process.env;
 const BASE_DIR = CI ? HOME : __dirname;
 const TEST_DIR = path.join(BASE_DIR, 'test-dir');
 
 describe('simple database', () => {
-
   beforeEach(async () => {
     await fs.rm(TEST_DIR, { force: true, recursive: true });
     await fs.mkdir(TEST_DIR, { recursive: true });
   });
 
-  it('needs a first test...', async () => {
-
-  });
-
+  it('needs a first test...', async () => {});
 });
